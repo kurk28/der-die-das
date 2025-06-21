6e3211a806569386d082d30a054bc1741be6a9b9
Ilia Dubovik <askdubovik@gmail.com>
2025-06-21 21:07:22 +0200

    feat: add start and select game buttons

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { createSignal, Match, Switch } from 'solid-js';
 import { Header } from './components/Header/Header';
 import { Game } from './components/Game/Game';
 import { WordsInput } from './components/WordsInput/WordsInput';
+import { Button } from './components/shared/Button/Button';
 
 export const App = () => {
   const [page, setPage] = createSignal(0);
@@ -15,11 +16,21 @@ export const App = () => {
         </div>
         <Switch fallback={<p>Not Found</p>}>
           <Match when={page() === 0}>
+            <div class={styles.chooseGameContainer}>
+              <Button color="blue" onClick={() => setPage(2)}>
+                Start new Game
+              </Button>
+              <Button color="green" onClick={() => setPage(3)}>
+                Select Game
+              </Button>
+            </div>
+          </Match>
+          <Match when={page() === 1}>
             <div class={styles.wordsInputContainer}>
               <WordsInput />
             </div>
           </Match>
-          <Match when={page() === 1}>
+          <Match when={page() === 2}>
             <div class={styles.gameContainer}>
               <Game />
             </div>
