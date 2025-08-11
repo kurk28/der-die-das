e536531967742f489c198cb8fd94b7bff6d07bde
Ilia Dubovik <askdubovik@gmail.com>
2025-08-11 16:05:11 +0200

    chore: eslint, prettier, playwright

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,26 @@
 import js from '@eslint/js';
+import solid from 'eslint-plugin-solid';
 import globals from 'globals';
 import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
   {
-    files: ['**/*.{js,mjs,cjs}'],
-    plugins: { js },
-    extends: ['js/recommended'],
-  },
-  {
-    files: ['**/*.{js,mjs,cjs}'],
+    files: ['**/*.{js,mjs,cjs,jsx}'],
+    plugins: { js, solid },
+    extends: ['js/recommended', 'solid/recommended'],
+    rules: {
+      'no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+    },
     languageOptions: {
       globals: {
         ...globals.browser,
         ...globals.node,
+      },
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
       },
     },
   },
